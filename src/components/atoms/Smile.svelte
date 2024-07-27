11420aa9250a89c7b0d997530566eb3a8437d344
itaak <dev@dev-itaak.com>
2024-07-27 23:39:23 +0200

    deploy

--- src/components/atoms/Smile.svelte
+++ src/components/atoms/Smile.svelte
@@ -17,7 +17,7 @@
     const gammaRange = 50;
 
     // Définir le point de repos pour beta et gamma
-    const restBeta = 0;
+    const restBeta = 30;
     const restGamma = 0;
 
     let x = baseX;
