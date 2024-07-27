e36e668d219961086618111b7b5baf6226c0de01
itaak <dev@dev-itaak.com>
2024-07-27 20:01:39 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://itaak.github.io/itaak.com',
+  site: 'https://itaak.github.io',
+  base: '/itaak.com/',
   integrations: [svelte()],
   output: 'static',
   i18n: {
